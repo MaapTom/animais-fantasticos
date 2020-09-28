7f8c83ccc3ce0378d4868d1450edb26fdb09fca2
Marco Antonio <marcoantoniopereira41@gmail.com>
2020-09-28 19:44:03 -0300

    ScrollSuave transformado em classe

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-import initScrollSuave from './modules/scroll-suave.js';
+import ScrollSuave from './modules/scroll-suave.js';
 import initAnimacaoScroll from './modules/scroll-animacao.js';
 import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
@@ -10,7 +10,8 @@ import initDate from './modules/date.js';
 import initFetchAnimais from './modules/fetch-animais.js';
 import initFetchBitCoin from './modules/fetch-bitCoin.js';
 
-initScrollSuave();
+const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
+scrollSuave.init();
 initAnimacaoScroll();
 initAccordion();
 initTabNav();
