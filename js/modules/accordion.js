9538f68fd56097a6c7b63e3920eb10e1871f1119
Marco Antonio <marcoantoniopereira41@gmail.com>
2020-09-28 21:24:30 -0300

    Accordion transformado em classe

--- js/modules/accordion.js
+++ js/modules/accordion.js
@@ -1,19 +1,26 @@
-export default function initAccordion() {
-  const accordionList = document.querySelectorAll('[data-anime="accordion"] dt');
-  const activeClass = 'ativo';
-
-  function activeAccordion() {
-    this.classList.toggle(activeClass);
-    this.nextElementSibling.classList.toggle(activeClass);
+export default class Accordion {
+  constructor(list) {
+    this.accordionList = document.querySelectorAll(list);
+    this.activeClass = 'ativo';
   }
 
-  if (accordionList.length) {
-    accordionList[0].classList.add(activeClass);
-    accordionList[0].nextElementSibling.classList.add(activeClass);
-    accordionList.forEach((item) => {
-      item.addEventListener('click', activeAccordion);
-    });
+  toggleAccordion(item) {
+    item.classList.toggle(this.activeClass);
+    item.nextElementSibling.classList.toggle(this.activeClass);
+  }
+
+  //adiciona os primeiros eventos ao accordion
+  addAccordionEvent(){
+    this.accordionList.forEach((item) => {
+      item.addEventListener('click', () => this.toggleAccordion(item));
+    })
+  }
+  //iniciar função
+  init(){
+    if (this.accordionList.length) {
+      //ativar primeiro item
+      this.toggleAccordion(this.accordionList[0])
+      this.addAccordionEvent();
+    }
   }
 }
-
-console.log('log');
--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 import ScrollSuave from './modules/scroll-suave.js';
-import initAnimacaoScroll from './modules/scroll-animacao.js';
-import initAccordion from './modules/accordion.js';
+import Accordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
 import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
@@ -9,11 +8,14 @@ import initMenuMobile from './modules/menu-mobile.js';
 import initDate from './modules/date.js';
 import initFetchAnimais from './modules/fetch-animais.js';
 import initFetchBitCoin from './modules/fetch-bitCoin.js';
+import initAnimacaoScroll from './modules/scroll-animacao.js';
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
-initAnimacaoScroll();
-initAccordion();
+
+const accordion = new Accordion('[data-anime="accordion"] dt');
+accordion.init();
+
 initTabNav();
 initModal();
 initTooltip();
@@ -22,3 +24,4 @@ initMenuMobile();
 initDate();
 initFetchAnimais();
 initFetchBitCoin();
+initAnimacaoScroll();
