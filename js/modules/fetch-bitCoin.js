c7c27ec9e36ac91e0d3c56c7447ca5cdfd9b7d52
Marco Antonio <marcoantoniopereira41@gmail.com>
2020-10-05 14:25:17 -0300

    fetch bitcoin refatorado

--- js/modules/fetch-bitCoin.js
+++ js/modules/fetch-bitCoin.js
@@ -1,8 +1,8 @@
-export default function initFetchBitCoin() {
-  fetch('https://blockchain.info/ticker')
+export default function fetchBitCoin(url, target) {
+  fetch(url)
     .then((response) => response.json())
     .then((json) => {
-      const btcPreco = document.querySelector('.btc-preco');
+      const btcPreco = document.querySelector(target);
       btcPreco.innerText = (1000 / json.BRL.sell).toFixed(4);
     }).catch((erro) => {
       console.log(erro);
